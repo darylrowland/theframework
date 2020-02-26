69e2dbfc73b0f3c257fc7d5d16c8ef8678da77f3
Daryl <daryl@darylrowland.co.uk>
2020-02-26 14:09:06 +0000

    Added docs

---
+++ docs/build/precache-manifest.08ff1d4e427dcfd7321ab0e717991b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782e1fe172803f2dd08a2a7ca06a8736",
+    "url": "docs/index.html"
+  },
+  {
+    "revision": "9af06600aa7fc09c6c78",
+    "url": "docs/static/css/main.8af79839.chunk.css"
+  },
+  {
+    "revision": "06cd730e19a7fe17c768",
+    "url": "docs/static/js/2.0b841cba.chunk.js"
+  },
+  {
+    "revision": "27df78d2822439c2fc110c0a385025d0",
+    "url": "docs/static/js/2.0b841cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af06600aa7fc09c6c78",
+    "url": "docs/static/js/main.f03a774d.chunk.js"
+  },
+  {
+    "revision": "3220c98b9c28e72ee759",
+    "url": "docs/static/js/runtime-main.bebb9c92.js"
+  }
+]);
